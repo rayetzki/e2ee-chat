beb73d27fd16b7eb947dadf436d9fd827e272ad5
Illia Raietskyi <illia.raietskyi.ext@vay.io>
2026-05-27 19:34:58 +0300

    Fix cookies options

--- src/routes/auth/+page.server.ts
+++ src/routes/auth/+page.server.ts
@@ -36,8 +36,8 @@ export const actions = {
         cookies.set(sessionCookieName, sessionId, {
             path: '/',
             httpOnly: true,
-            secure: isSecure,
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'lax',
             maxAge: 60 * 60 * 1,
         });
 
@@ -46,8 +46,8 @@ export const actions = {
         cookies.set(chatCookieName, hashedChatId, {
             path: '/',
             httpOnly: true,
-            secure: isSecure,
-            sameSite: 'strict',
+            secure: true,
+            sameSite: 'lax',
             maxAge: 60 * 60 * 1,
         });
 
